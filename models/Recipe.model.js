cff0218a44cd883d49ef1f73220ffdbb7536039b
Sebastian Laca <seba.laca@gmail.com>
2022-06-23 16:23:01 +0200

    Iteracion 1
    Completa

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,51 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+
+  ingredients: {
+    type: [String],
+    required: true
+  },
+
+  cuisine: {
+    type: String,
+    require: true,
+  },
+
+  dishType: {
+    type: String,
+    enum:['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'],
+    default:'other'
+  },
+
+  image: {
+    type: String,
+    default:  "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+
+  duration: {
+    type: Number,
+  },
+
+  creator: {
+    type: String,
+  },
+
+  created: {
+    type: Date,
+    default: Date.now()
+  }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
